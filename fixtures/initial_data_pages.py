9c46227639600c36b6b95f51fc1d6d14d6ff6d83
manojlds <manojlds@gmail.com>
2013-03-17 12:39:44 +0530

    Adding inital page setup

---
+++ fixtures/initial_data_pages.py
@@ -0,0 +1,31 @@
+[
+  {
+    "pk": 1,
+    "model": "pages.page",
+    "fields": {
+      "status": 2,
+      "_order": 0,
+      "titles": "Home",
+      "description": "Home",
+      "parent": null,
+      "title": "Home",
+      "short_url": null,
+      "login_required": false,
+      "site": 1,
+      "in_sitemap": false,
+      "keywords_string": "",
+      "_meta_title": null,
+      "expiry_date": null,
+      "publish_date": "2013-03-13T14:11:54Z",
+      "content_model": "link",
+      "in_menus": "",
+      "slug": "/",
+      "gen_description": true
+    }
+  },
+  {
+    "pk": 1,
+    "model": "pages.link",
+    "fields": {}
+  }
+]
