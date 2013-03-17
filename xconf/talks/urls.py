5ad69af940046b46996d58d74b1d5fac6c25f0ae
manojlds <manojlds@gmail.com>
2013-03-17 13:13:06 +0530

    Adding auth to the browseable api

--- xconf/talks/urls.py
+++ xconf/talks/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import TalkList, TalkDetail, VoteList, VoteDetail
 
 urlpatterns = patterns('xconf.talks.views',
     url(r'^$', 'api_root'),
+    url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^talks/$', TalkList.as_view(), name='blogpost-list'),
     url(r'^talks/$', TalkList.as_view(), name='talk-list'),
     url(r'^talks/(?P<pk>\d+)/$', TalkDetail.as_view(), name='blogpost-detail'),
