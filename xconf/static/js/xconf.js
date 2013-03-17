e8115525efb375d5f220fe3a02a41aac459a957f
Krishnaswamy Subramanian <jskswamy@gmail.com>
2013-03-17 16:07:45 +0530

    Using knockout and underscore to wire up with data

--- xconf/static/js/xconf.js
+++ xconf/static/js/xconf.js
@@ -9,4 +9,36 @@ $(function(){
             return element.data('top');
         }}
     });
+    Vote.init();
 });
+
+var Vote = {
+    init: function() {
+        $.getJSON('/talks/api/talks/', function(talks) {
+        })
+    }
+}
+
+var talk = function(title, category, votes) {
+        var self=this;
+        self.title = title;
+        self.category = category;
+        self.votes = ko.observable(votes);
+    };
+
+var user = function() {
+    var self = this;
+    self.selectedTopics = ko.observableArray();
+    self.canVote = ko.computed(function() {
+        return self.selectedTopics().length < 3 ;}, self);
+};
+
+
+var votableViewModel = function() {
+    var self = this;
+    self.talks = ko.observableArray();
+    self.user = ko.observable(new user());
+};
+
+var xconf = new votableViewModel();
+ko.applyBindings(xconf);
