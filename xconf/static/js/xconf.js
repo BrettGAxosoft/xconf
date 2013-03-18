edebf1a1a4710cf86231dca2bdc320a300693d21
Krishnaswamy Subramanian <jskswamy@gmail.com>
2013-03-18 21:39:26 +0530

    Replacing knockout with angular

--- xconf/static/js/xconf.js
+++ xconf/static/js/xconf.js
@@ -1,34 +1,60 @@
-$(function(){
-    Vote.init();
+angular.module('services', ['ngResource']).
+    factory('Category', function($resource) {
+         return $resource('/talks/api/categories');
+    }).
+    factory('Talk', function($resource) {
+         return $resource('/talks/api/categories/:categoryId/talks', {categoryId: 'categoryId'});
+    });
+
+angular.module('xconf', ['services']).config(function($interpolateProvider) {
+    $interpolateProvider.startSymbol('[[');
+    $interpolateProvider.endSymbol(']]');
+}).filter('startFrom', function() {
+    return function(input, start) {
+        start = +start; //parse to int
+        return input.slice(start);
+    };
 });
 
-var Vote = {
-    init: function() {
-        $.getJSON('/talks/api/talks/', function(talks) {    
-        })
-    }
-}
+var XConfCtrl = ["$scope", "Category", "Talk", function($scope, Category, Talk){
+    Category.get(function(data){
+        data.results = _(data.results).map(function(category){
+            category.hyphanizedTitle = hyphanize(category.title);
+            return category;
+        });
+        $scope.categories = data;
+        $scope.selectedCategory = data.results[0];
+    });
 
-var talk = function(title, category, votes) {
-        var self=this;
-        self.title = title;
-        self.category = category;
-        self.votes = ko.observable(votes);
+    var hyphanize = function(string) {
+        return string.replace(' ','-');
     };
 
-var user = function() {
-    var self = this;
-    self.selectedTopics = ko.observableArray();
-    self.canVote = ko.computed(function() {
-        return self.selectedTopics().length < 3 ;}, self);
-};
+    $scope.currentPage = 1;
+    $scope.pageSize = 4;
 
+    $scope.talks = {results: []};
 
-var votableViewModel = function() {
-    var self = this;
-    self.talks = ko.observableArray();
-    self.user = ko.observable(new user());
-};
+    $scope.loadTalks = function(category){
+        Talk.get({categoryId: category.id, page_size: $scope.pageSize, page: $scope.currentPage}, function(data){
+            $scope.talks = data;
+            $scope.nextAvailable = data.next;
+            $scope.previousAvailable = data.previous;
+        });
+    };
 
-var xconf = new votableViewModel();
-ko.applyBindings(xconf);
+    $scope.switchTalks = function(category) {
+        $scope.currentPage = 1;
+        $scope.loadTalks(category);        
+    }
+
+    $scope.nextPage = function(category){
+        $scope.currentPage += 1;
+        $scope.loadTalks(category);
+    };
+
+    $scope.previousPage = function(category) {
+        $scope.currentPage -= 1;
+        $scope.loadTalks(category);
+    };
+}];
