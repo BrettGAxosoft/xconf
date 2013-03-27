0a202bd9941fb3ee32e1f8a6ba31a43b9bf8ebe0
manojlds <manojlds@gmail.com>
2013-03-28 01:45:40 +0530

    BROKEN - refactoring to use generated rows

--- xconf/static/js/xconf.js
+++ xconf/static/js/xconf.js
@@ -24,6 +24,25 @@ angular.module('xconf', ['services']).config(function($interpolateProvider) {
         start = +start; //parse to int
         return input.slice(start);
     };
+}).filter('groupBy', function() {
+    return function(items, groupedBy) {
+        var finalItems = [],
+            thisGroup;
+        for (var i = 0; i < items.length; i++) {
+            if (!thisGroup) {
+                thisGroup = [];
+            }
+            thisGroup.push(items[i]);
+            if (((i+1) % groupedBy) == 0) {
+                finalItems.push(thisGroup);
+                thisGroup = null;
+            }
+        };
+        if (thisGroup) {
+            finalItems.push(thisGroup);
+        }
+        return finalItems;
+    };
 }).config(function($httpProvider) {
     $httpProvider.defaults.transformRequest.push(function(data, headersGetter) {
         var headers = headersGetter();
