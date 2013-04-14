ab498f670301d333b4e168ae1d57dd7089cd2fa5
manojlds <manojlds@gmail.com>
2013-04-15 00:27:33 +0530

    Adding schedule and tracks

--- xconf/leap/admin.py
+++ xconf/leap/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
+
+from mezzanine.core.admin import TabularDynamicInlineAdmin
+
 from .models import Type, Slot, Track, Schedule
 
 
-class SlotInline(admin.TabularInline):
+class SlotInline(TabularDynamicInlineAdmin):
     model = Slot
-
+    extra = 10
 
 class TrackAdmin(admin.ModelAdmin):
     inlines = [
@@ -25,4 +28,4 @@ class ScheduleAdmin(admin.ModelAdmin):
 admin.site.register(Type)
 admin.site.register(Slot)
 admin.site.register(Track, TrackAdmin)
-admin.site.register(Schedule, TrackAdmin)
+admin.site.register(Schedule, ScheduleAdmin)
--- xconf/leap/models.py
+++ xconf/leap/models.py
@@ -4,10 +4,16 @@ from django.db import models
 class Schedule(models.Model):
     conference = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.conference
+
 
 class Track(models.Model):
     name = models.CharField(max_length=30)
     schedule = models.ForeignKey(Schedule, related_name='tracks')
+
+    def __str__(self):
+        return self.name
 
 
 class Type(models.Model):
@@ -22,3 +28,6 @@ class Slot(models.Model):
     track = models.ForeignKey(Track, related_name='slots')
     type = models.ForeignKey(Type, related_name='slots')
     start_time = models.TimeField()
+
+    def __str__(self):
+        return "{0} ({1})".format(self.start_time, self.type)
