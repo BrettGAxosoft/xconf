3cc38b6796752f52f59bcc4b43d516073344241a
manojlds <manojlds@gmail.com>
2013-04-16 11:19:30 +0530

    Styling the slots view

--- xconf/leap/views.py
+++ xconf/leap/views.py
@@ -7,7 +7,10 @@ from .models import Track
 
 def index(request):
     templates = ["leap/index.html"]
+    track1 = Track.objects.get(pk=1)
+    track2 = Track.objects.get(pk=2)
+    track3 = Track.objects.get(pk=3)
     c = Context({
-            'tracks': Track.objects.all()
+            'slots': zip(track1.slots.all(), track2.slots.all(), track3.slots.all())
         })
     return render(request, templates, c)
